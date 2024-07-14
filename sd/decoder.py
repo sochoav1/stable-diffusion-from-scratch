d346115de297dd5d417ebfc8e8c19f6d42f08c71
Santiago Ochoa <123663913+sochoav1@users.noreply.github.com>
2024-07-14 08:29:37 +0000

    add decoder.py

--- sd/decoder.py
+++ sd/decoder.py
@@ -3,6 +3,34 @@ from torch import nn
 from torch.nn import functional as F
 from attention import selfAttention
 
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.group_norm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        #(Batch_size, features, height, width)
+        
+        residue = x
+        
+        n, c, h, w = x.shape
+        
+        # (Batch_size, features, height, width) --> (Batch_size, features, height * width)
+        x = x.view(n, c, h * w)
+        
+        #(Batch_size, features, height * width) --> (Batch_size, height * width, features)
+        x = x.transpose = (-1, -2)
+        
+        #(Batch_size, height * width, features) --> (Batch_size, height * width, features)
+        x = self.attention(x)
+        #(Batch_size, height * width, features) --> (Batch_size, features, height * width)
+        x = x.transpose(-1, -2)
+        
+        x *= residue
+        
+        return x
+
 class VAE_ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
@@ -12,8 +40,21 @@ class VAE_ResidualBlock(nn.Module):
         self.group_norm2 = nn.GroupNorm(32, out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
         
-        if in_channels = out_channels:
+        if in_channels == out_channels:
             self.residual_layer = nn.Identity()
+        
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
             
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+            # x = (B, C, H, W) batch, channels, height, width
+            residual = x
             
+            x = self.group_norm1(x)
+            x = F.silu(x)
+            x = self.conv1(x)
+            x = self.group_norm2(x)
+            x = F.silu(x)
+            x = self.conv2(x)
             
+            return x + self.residual_layer(residual)
--- sd/encoder.py
+++ sd/encoder.py
@@ -1,1 +1,85 @@
-import rtorch 
+import torch
+from decoder import VAE_attentionBlock, VAE_ResidualBlock
+from torch import nn
+from torch.nn import functional as F
+
+
+class VAE_encoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            # Batch_size, Channel, Height, Width) --> (Batch_size, 128, height, width)
+            nn.Conv2d(3, 128, padding=1),
+            
+            #(Batch_size, 128, height, width) --> (Batch_size, 128, height  , width)
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            
+            #(Batch_size, 128, height, width) --> (Batch_size, 128, height/2, width/2)
+            nn.Conv2d(128, 128, kernel_size= 3, stride= 2, padding= 0),
+            
+            #(Batch_size, 128, height/2, width/2) --> (Batch_size, 256, height/2, width/2)
+            VAE_ResidualBlock(128, 256),
+            
+            #(Batch_size, 256, height/2, width/2) --> (Batch_size, 256, height/2, width/2)
+            VAE_ResidualBlock(256, 256),
+            
+            # (Batch_size, 256, height/2, width/2) --> (Batch_size, 256, height/4, width/4)
+            nn.Conv2d(256, 256, kernel_size= 3, stride= 2, padding= 0),
+            
+            #(Batch_size, 256, height/4, width/4) --> (Batch_size, 512, height/4, width/4)
+            VAE_ResidualBlock(256, 512),
+            
+            #(Batch_size, 512, height/4, width/4) --> (Batch_size, 512, height/4, width/4)
+            VAE_ResidualBlock(512, 512),
+            
+            #(Batch_size, 512, height/4, width/4) --> (Batch_size, 512, height/8, width/8)
+            nn.Conv2d(512, 512, kernel_size= 3, stride= 2, padding= 0),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            
+            #(Batch_size, 512, height/8, width/8) --> (Batch_size, 512, height/8, width/8)
+            VAE_ResidualBlock(512, 512),
+            
+            #(Batch_size, 512, height/8, width/8) --> (Batch_size, 512, height/8, width/8)
+            VAE_attentionBlock(512, 512),
+            
+            #(Batch_size, 512, height/8, width/8) --> (Batch_size, 512, height/8, width/8)
+            nn.Group(32, 512),
+            nn.SiLU(),
+            
+            #(Batch_size, 512, height/8, width/8) --> (Batch_size, 8, height/8, width/8)
+            nn.Conv2d(512, 8, kernel_size= 1, padding= 1),
+            
+            #(Batch_size, 8, height/8, width/8) --> (Batch_size, 8, height/8, width/8)
+            nn.Conv2d(8, 8, kernel_size= 1, padding= 1)
+        )
+        
+    def forward(self, x: torch.Tensor, noise: torch.Tensor) -> torch.Tensor:
+        #X = (Batch_size, 3, height, width) 
+        #noise =  (Batch_size, Out_Channel, height/8, width/8)
+        
+        for module in self:
+            if getattr(module, 'stride', None) == (2, 2):
+                
+                #(padding_left, padding_right, padding_top, padding_bottom) (0, 1, 0, 1) asimetrical padding
+                x = F.pad(x, (0, 1, 0, 1))
+            x = module(x)
+    
+        #(Batch_size, 8, height/8, width/8) --> Two tensor of shape (Batch_size, 4, height/8, width/8)
+        mean, log_variance = torch.chunk(x, 2, dim = 1)
+        
+        log_variance = torch.clamp(log_variance, min = -30, max = 20)
+        
+        variance = torch.exp(log_variance)
+        
+        std = torch.sqrt(variance)
+        
+        #Z(0, 1) = N(mean, std) = x?
+        # X = mean + std * noise
+        
+        x = mean + std * noise
+        
+        #Scale by constant (taken from the paper)
+        x *= 0.18215
+        
+        return x
