acd22af8e6791cc389c71ef2255b26114b2eff9e
Santiago Ochoa <123663913+sochoav1@users.noreply.github.com>
2024-07-14 08:09:19 +0000

    encoder .py

---
+++ sd/decoder.py
@@ -0,0 +1,19 @@
+import torch 
+from torch import nn
+from torch.nn import functional as F
+from attention import selfAttention
+
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.group_norm1 = nn.GroupNorm(32, in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        
+        self.group_norm2 = nn.GroupNorm(32, out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        
+        if in_channels = out_channels:
+            self.residual_layer = nn.Identity()
+            
+            
+            
